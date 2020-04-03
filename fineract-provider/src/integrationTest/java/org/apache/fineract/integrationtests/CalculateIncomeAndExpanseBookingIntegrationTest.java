d4ac3aed5c36381a03227cb0b7adcc49b2ce9fe4
Karan Singh <singkara@yorku.ca>
2020-04-03 03:55:04 -0400

    FINERACT-38 : Renaming and Updating test:

--- fineract-provider/src/integrationTest/java/org/apache/fineract/integrationtests/CalculateIncomeAndExpanseBookingIntegrationTest.java
+++ fineract-provider/src/integrationTest/java/org/apache/fineract/integrationtests/CalculateIncomeAndExpanseBookingIntegrationTest.java
@@ -23,79 +23,74 @@ import org.apache.fineract.accounting.closure.command.GLClosureCommand;
 import org.apache.fineract.accounting.closure.data.IncomeAndExpenseJournalEntryData;
 import org.apache.fineract.accounting.closure.domain.GLClosureRepository;
 import org.apache.fineract.accounting.closure.exception.RunningBalanceNotCalculatedException;
-import org.apache.fineract.accounting.closure.serialization.GLClosureCommandFromApiJsonDeserializer;
 import org.apache.fineract.accounting.closure.service.CalculateIncomeAndExpenseBookingImpl;
 import org.apache.fineract.accounting.closure.service.IncomeAndExpenseReadPlatformService;
 import org.apache.fineract.accounting.glaccount.domain.GLAccountRepositoryWrapper;
-import org.apache.fineract.infrastructure.core.api.JsonQuery;
+import org.apache.fineract.integrationtests.common.Utils;
 import org.apache.fineract.organisation.office.domain.OfficeRepositoryWrapper;
 import org.apache.fineract.organisation.office.service.OfficeReadPlatformService;
+import org.junit.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
 import org.joda.time.LocalDate;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-/**
- * JUnit Test Cases for Account Transfer for.
- */
-public class CalculateIncomeAndExpanseBookingImplTest {
-
+public class CalculateIncomeAndExpanseBookingIntegrationTest {
     /*
     Test class CalculateIncomeAndExpenseBookingImpl
      */
-    @Autowired
+
+    @Mock private OfficeRepositoryWrapper officeRepository;
+    @Mock private GLClosureRepository glClosureRepository;
+    @Mock private GLAccountRepositoryWrapper glAccountRepository;
+    @Mock private IncomeAndExpenseReadPlatformService incomeAndExpenseReadPlatformService;
+    @Mock private OfficeReadPlatformService officeReadPlatformService;
+
+    @InjectMocks
     private CalculateIncomeAndExpenseBookingImpl calculateIncomeAndExpenseBooking;
-    private GLClosureCommandFromApiJsonDeserializer fromApiJsonDeserializer;
-    private OfficeRepositoryWrapper officeRepository;
-    private GLClosureRepository glClosureRepository;
-    private GLAccountRepositoryWrapper glAccountRepository;
-    private IncomeAndExpenseReadPlatformService incomeAndExpenseReadPlatformService;
-    private OfficeReadPlatformService officeReadPlatformService;
 
     @Before
     public void setup() {
+        calculateIncomeAndExpenseBooking =  new CalculateIncomeAndExpenseBookingImpl(null, officeRepository, glClosureRepository, glAccountRepository, incomeAndExpenseReadPlatformService,officeReadPlatformService);
+
     }
 
     @After
     public void tearDown() {
-
     }
-
     /*
-    Case 1: All running balances has to be calculated before booking off income and expense account
-    No exception is to be thrown since it is running for the input.
-    However, later a null pointer exception is thrown, since the object incomeAndExpense is not initialized.
-         */
-    @Test(expected = Exception.class)
-    public void testBookOffIncomeAndExpense() {
-        JsonQuery query = new JsonQuery("select command to extract income and expanse", null, null);
-        GLClosureCommand glClosureCommand =  new GLClosureCommand((long)10,(long)10, new LocalDate(),"Closing comment",  false, (long)10 , "CAD", false, false, "comment" );
-        IncomeAndExpenseJournalEntryData incomeAndExpenseJournalEntryData= new IncomeAndExpenseJournalEntryData(null, null,null, null
-        , true, true, null,null,null,10,10,null,null);
-        List<IncomeAndExpenseJournalEntryData> incomeAndExpenseJournalEntryDataList = new ArrayList<>();
-        incomeAndExpenseJournalEntryDataList.add(incomeAndExpenseJournalEntryData);
-        calculateIncomeAndExpenseBooking.bookOffIncomeAndExpense(incomeAndExpenseJournalEntryDataList, glClosureCommand, false,null,null);
-
-    }
-
-    /*
-        Case 2: All running balances has to be calculated before booking off income and expense account
+        Case 1: All running balances has to be calculated before booking off income and expense account
         If not running balances, then throw exception
     */
     @Test(expected = RunningBalanceNotCalculatedException.class)
     public void testBookOffIncomeAndExpenseCheckException() {
-        JsonQuery query = new JsonQuery("select command to extract income and expanse", null, null);
         GLClosureCommand glClosureCommand =  new GLClosureCommand((long)10,(long)10, new LocalDate(),"Closing comment",  false, (long)10 , "CAD", false, false, "comment" );
         IncomeAndExpenseJournalEntryData incomeAndExpenseJournalEntryData= new IncomeAndExpenseJournalEntryData(null, null,null, null
-                , true, true, null,null,null,10,10,null,null);
+                , true, false, null,null,null,10,10,null,null);
         List<IncomeAndExpenseJournalEntryData> incomeAndExpenseJournalEntryDataList = new ArrayList<IncomeAndExpenseJournalEntryData>();
         incomeAndExpenseJournalEntryDataList.add(incomeAndExpenseJournalEntryData);
         calculateIncomeAndExpenseBooking.bookOffIncomeAndExpense(incomeAndExpenseJournalEntryDataList,glClosureCommand, false,null,null);
-
     }
+
+    /*
+    Case 2: All running balances has to be calculated before booking off income and expense account
+    No exception is to be thrown since it is running for the input.
+    However, later a null pointer exception is thrown, since the object incomeAndExpense is not initialized.
+         */
+    @Test
+    public void testBookOffIncomeAndExpense() {
+        GLClosureCommand glClosureCommand =  new GLClosureCommand(10L, 10L, new LocalDate(), "Closing comment", false, 10L , "CAD", false, false, "comment" );
+        IncomeAndExpenseJournalEntryData incomeAndExpenseJournalEntryData= new IncomeAndExpenseJournalEntryData(null, null,null, null
+                , true, true, null,null,null,10,10,null,null);
+        List<IncomeAndExpenseJournalEntryData> incomeAndExpenseJournalEntryDataList = new ArrayList<>();
+        incomeAndExpenseJournalEntryDataList.add(incomeAndExpenseJournalEntryData);
+        Assert.assertNotNull(calculateIncomeAndExpenseBooking.bookOffIncomeAndExpense(incomeAndExpenseJournalEntryDataList, glClosureCommand, false,null,null));
+    }
+
+
 
 }
