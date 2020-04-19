0e1da45c801676576a8b8830999ec97ee94487a8
Restyled.io <commits@restyled.io>
2020-04-19 23:02:49 +0000

    Restyled by shfmt

--- kubernetes/kubectl-startup.sh
+++ kubernetes/kubectl-startup.sh
@@ -28,13 +28,13 @@ kubectl apply -f fineractmysql-deployment.yml
 
 fineractmysql_pod=""
 while [[ ${#fineractmysql_pod} -eq 0 ]]; do
-    fineractmysql_pod=$(kubectl get pods -l tier=fineractmysql --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+  fineractmysql_pod=$(kubectl get pods -l tier=fineractmysql --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
 done
 
 fineractmysql_status=$(kubectl get pods "$fineractmysql_pod" --no-headers -o custom-columns=":status.phase")
 while [[ ${fineractmysql_status} -ne 'Running' ]]; do
-    sleep 1
-    fineractmysql_status=$(kubectl get pods "$fineractmysql_pod" --no-headers -o custom-columns=":status.phase")
+  sleep 1
+  fineractmysql_status=$(kubectl get pods "$fineractmysql_pod" --no-headers -o custom-columns=":status.phase")
 done
 
 echo
@@ -43,13 +43,13 @@ kubectl apply -f fineract-server-deployment.yml
 
 fineract_server_pod=""
 while [[ ${#fineract_server_pod} -eq 0 ]]; do
-    fineract_server_pod=$(kubectl get pods -l tier=backend --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+  fineract_server_pod=$(kubectl get pods -l tier=backend --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
 done
 
 fineract_server_status=$(kubectl get pods "$fineract_server_pod" --no-headers -o custom-columns=":status.phase")
 while [[ ${fineract_server_status} -ne 'Running' ]]; do
-    sleep 1
-    fineract_server_status=$(kubectl get pods "$fineract_server_pod" --no-headers -o custom-columns=":status.phase")
+  sleep 1
+  fineract_server_status=$(kubectl get pods "$fineract_server_pod" --no-headers -o custom-columns=":status.phase")
 done
 
 echo "Fineract server is up and running"
