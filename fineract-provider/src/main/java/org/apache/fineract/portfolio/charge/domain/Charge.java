9a6baed89d2833528384e8c74e68511c6290cd0b
Saransh Sharma <marktwain@gmx.us>
2020-05-08 00:15:16 +0530

    Added New Charge Type Fix

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/charge/domain/Charge.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/charge/domain/Charge.java
@@ -197,7 +197,7 @@ public class Charge extends AbstractPersistableCustom {
                 baseDataValidator.reset().parameter("chargeCalculationType").value(this.chargeCalculation)
                         .failWithCodeNoParameterAddedToErrorCode("not.allowed.charge.calculation.type.for.savings");
             }
-            
+
             if (!(ChargeTimeType.fromInt(getChargeTimeType()).isWithdrawalFee() || ChargeTimeType.fromInt(getChargeTimeType()).isSavingsInternalTransfer() ||ChargeTimeType.fromInt(getChargeTimeType()).isSavingsNoActivityFee())
                     && ChargeCalculationType.fromInt(getChargeCalculation()).isPercentageOfAmount()) {
                 baseDataValidator.reset().parameter("chargeCalculationType").value(this.chargeCalculation)
