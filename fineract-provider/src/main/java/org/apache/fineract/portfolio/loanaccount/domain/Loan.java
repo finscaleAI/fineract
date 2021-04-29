c5a056acfeba2b5903aa2135a138f98a5eb0d0ba
Audrey Ndum <audreyndum@gmail.com>
2021-04-29 01:01:44 +0100

    pre-approval consider only charges with disbursement time type

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/Loan.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/Loan.java
@@ -44,6 +44,7 @@ import java.util.ListIterator;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
@@ -526,7 +527,9 @@ public class Loan extends AbstractPersistableCustom {
         // Add net get net disbursal amount from charges and principal
         this.netDisbursalAmount = this.approvedPrincipal;
         if (loanCharges != null && !loanCharges.isEmpty()) {
-            for (LoanCharge charge : loanCharges) {
+            Set<LoanCharge> chargesAtDisbursal = loanCharges.stream().filter(charge -> charge.isDueAtDisbursement())
+                    .collect(Collectors.toSet());
+            for (LoanCharge charge : chargesAtDisbursal) {
                 this.netDisbursalAmount = this.netDisbursalAmount.subtract(charge.amount());
             }
         }
