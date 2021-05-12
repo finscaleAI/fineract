9099817fb9183ab12476e4201a74067ce5ea0cce
Audrey <40563356+Cadreia@users.noreply.github.com>
2021-05-12 01:29:32 +0100

    Revert "NDA improvements"

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/Loan.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/Loan.java
@@ -44,7 +44,6 @@ import java.util.ListIterator;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
@@ -527,9 +526,7 @@ public class Loan extends AbstractPersistableCustom {
         // Add net get net disbursal amount from charges and principal
         this.netDisbursalAmount = this.approvedPrincipal;
         if (loanCharges != null && !loanCharges.isEmpty()) {
-            Set<LoanCharge> chargesAtDisbursal = loanCharges.stream().filter(charge -> charge.isDueAtDisbursement())
-                    .collect(Collectors.toSet());
-            for (LoanCharge charge : chargesAtDisbursal) {
+            for (LoanCharge charge : loanCharges) {
                 this.netDisbursalAmount = this.netDisbursalAmount.subtract(charge.amount());
             }
         }
